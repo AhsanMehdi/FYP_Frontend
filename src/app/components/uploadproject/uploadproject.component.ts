7b4f25c648bfa37a4bd30d9100c1fafcbfa216aa
Ahsan Mehdi <mmahsanmehdi@gmail.com>
2020-12-20 11:49:01 +0500

    Feature post project was added

--- src/app/components/uploadproject/uploadproject.component.ts
+++ src/app/components/uploadproject/uploadproject.component.ts
@@ -24,20 +24,75 @@ export class UploadProjectComponent implements OnInit {
   Loucation = "";
   Type = "";
 
+  ShowTab1 = true;
+  ShowTab2= false;
+  ShowTab3= false;
+
+  Tab1state : string = "completed"
+  Tab2state : string = ""
+  Tab3state : string = ""
+
+  validatingForm: FormGroup;
+
+
   constructor(
     private route: ActivatedRoute,
     private router: Router,
     private backendService: BackendService,
-    private alertService: AlertService
+    private alertService: AlertService,
+    private fb: FormBuilder,
   ) {
     this.Title = this.route.snapshot.queryParams.title;
     this.Loucation = this.route.snapshot.queryParams.loucation;
     this.Type = this.route.snapshot.queryParams.type;
+
+    this.validatingForm = fb.group({
+      objective: new FormControl({value: '', disabled: false}, [
+        Validators.required,
+        Validators.minLength(4),
+      ]),
+
+      
+      title: new FormControl({value: this.Title, disabled: false}, [
+        Validators.required,
+        Validators.minLength(4),
+      ]),
+      loucation: new FormControl({value: this.Loucation, disabled: false}, [
+        Validators.required,
+        Validators.minLength(4),
+      ]),
+      type: new FormControl({value: this.Type, disabled: false}, [
+        Validators.required,
+        Validators.minLength(4),
+      ]),
+
+    });
 
     console.log(this.Title, this.Loucation, this.Type)
   }
 
   ngOnInit(): void {
   }
+
+  postproject(): void{
+
+  }
+
+  onSubmit(): void{
+    
+  }
+
+  onNext(section: string): void{
+
+    if (section == '2'){
+      this.ShowTab1= false
+      this.ShowTab2 = true
+      this.Tab2state="completed"
+    }
+  
+    
+  }
+
+  
 
 }
