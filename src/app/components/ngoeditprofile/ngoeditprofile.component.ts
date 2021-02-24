349de91f099b8050767e7767183414c6f6d56b9e
AhsanMehdi <62194299+AhsanMehdi@users.noreply.github.com>
2021-02-24 11:46:34 +0500

    projectupload&ngoeditprofiledone

--- src/app/components/ngoeditprofile/ngoeditprofile.component.ts
+++ src/app/components/ngoeditprofile/ngoeditprofile.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { BackendService } from '../../services/backend.service';
+import { AlertService } from '../../services/alert.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-ngoeditprofile',
@@ -7,9 +12,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NgoeditprofileComponent implements OnInit {
 
-  constructor() { }
+  NgoeditProfileForm: FormGroup;
 
-  ngOnInit(): void {
+  
+  loading = false;
+  submitted = false;
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private backendService: BackendService,
+    private alertService: AlertService
+  ) { }
+ /* function called on the call of html file */
+  ngOnInit()  {
+    this.NgoeditProfileForm = this.formBuilder.group({
+   
+      firstName: ['', Validators.required],  
+      middleName: ['',Validators.required],
+      lastName: ['', [Validators.required, Validators.minLength(12)]],
+      dob: ['', [Validators.required, Validators.minLength(12)]],
+      cellNumber: ['', Validators.required],
+      interestedDomain: ['', Validators.required],
+      cnic: ['', Validators.required],
+      country: ['', Validators.required],
+      visibility: ['', Validators.required],
+      occupation: ['', Validators.required],
+      domainHealth:  ['', Validators.required],
+      domainEducation:  ['', Validators.required],
+      domainOrphanage:  ['', Validators.required],
+      domainEnvironment:  ['', Validators.required],
+      domainSocialWelfare:  ['', Validators.required],
+      domainOther:  ['', Validators.required]
+  });
+
   }
 
+  /* this function should be used in html file to get data*/ 
+  onNgoEditProfileSubmit() {
+
+    console.log(this.NgoeditProfileForm.value)
+    this.submitted = true;
+    // stop here if form is invalid
+    // if (this.registerUserForm.invalid) {
+    //     return;
+        
+    // }
+    this.loading = true;
+    this.backendService.donorProfile(this.NgoeditProfileForm.value)
+        .pipe(first())
+        .subscribe(
+            data => {
+                this.alertService.success('Registration successful', true);
+                this.router.navigate(['/donorhome']);
+              
+            },
+            error => {
+                this.alertService.error(error);
+                this.loading = false;
+            });
+  }
 }
--- src/app/components/projectupload/projectupload.component.ts
+++ src/app/components/projectupload/projectupload.component.ts
@@ -1,4 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,EventEmitter } from '@angular/core';
+import { BackendService } from "../../services/backend.service";
+import { AlertService } from "../../services/alert.service";
+import { ToastrService } from 'ngx-toastr';
+import {
+  NgForm,
+  FormControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from "@angular/forms";
+import { first } from "rxjs/operators";
+import { IProject } from "../../_models/Iproject";
+import { Router, ActivatedRoute } from "@angular/router";
+import { UploadFile, UploadInput, UploadOutput } from "ng-uikit-pro-standard";
+import { IMyOptions, humanizeBytes } from "ng-uikit-pro-standard";
 
 @Component({
   selector: 'app-projectupload',
@@ -7,9 +22,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectuploadComponent implements OnInit {
 
-  constructor() { }
+  formData: FormData;
+  files: UploadFile[];
+  uploadInput: EventEmitter<UploadInput>;
+  humanizeBytes: Function;
+  dragOver: boolean;
+  
+  createProjectForm ; 
+  validatingForm: FormGroup; /*for validations*/
+ 
+  loading = false;
+  submitted = false;
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private backendService: BackendService,
+    private alertService: AlertService,
+    private formBuilder: FormBuilder,
+    private toastr: ToastrService
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.createProjectForm = this.formBuilder.group({
+   
+      nickName: ['', Validators.required],  
+      subject: ['',Validators.required],
+      descriptionStory: ['', [Validators.required, Validators.minLength(12)]],
+      objective: ['', [Validators.required, Validators.minLength(12)]],
+      country: ['', Validators.required],
+      status: ['', Validators.required],
+      dateOfCreation: ['', Validators.required],
+      imageUrl: ['', Validators.required]
+  });
+  }/* ngoninint ends*/
+
+  onCreateProjectSubmit() {
+
+    console.log(this.createProjectForm.value)
+    this.submitted = true;
+    // stop here if form is invalid
+    // if (this.registerUserForm.invalid) {
+    //     return;
+        
+    // }
+    this.loading = true;
+    this.backendService.createProject(this.createProjectForm.value)
+        .pipe(first())
+        .subscribe(
+            data => {
+                this.alertService.success('Campaign Created Successfully', true);
+                this.router.navigate(['/donorhome']);
+              
+            },
+            error => {
+                this.alertService.error(error);
+                this.loading = false;
+            });
   }
 
 }
