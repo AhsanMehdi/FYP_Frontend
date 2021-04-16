de164ff7ff9495a18141c9ab2433e9365afcb03f
dope4k <61405415+dope4k@users.noreply.github.com>
2021-04-16 22:58:47 +0500

    nsettingsupdated

--- src/app/components/nsetting/nsetting.component.ts
+++ src/app/components/nsetting/nsetting.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { BackendService } from '../../services/backend.service';
+import { AlertService } from '../../services/alert.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
+import { Router } from '@angular/router'
+import { IDonorProfile } from "../../_models/Idonorprofile";
 
 @Component({
   selector: 'app-nsetting',
@@ -6,10 +12,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nsetting.component.scss']
 })
 export class NsettingComponent implements OnInit {
+  editProfileForm: FormGroup;
+  validatingForm: FormGroup; /*for validations*/
 
-  constructor() { }
+  
+  loading = false;
+  submitted = false;
+  constructor(private formBuilder: FormBuilder,
+    private router: Router,
+    private backendService: BackendService,
+    private alertService: AlertService) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit()  {
+      this.editProfileForm = this.formBuilder.group({
+     
+        userName: ['', Validators.required],  
+        password: ['',Validators.required, Validators.minLength(8)],
+        });
+  
+    }
+    onEditProfileSubmit() {
 
+      console.log(this.editProfileForm.value)
+      this.submitted = true;
+      // stop here if form is invalid
+      // if (this.registerUserForm.invalid) {
+      //     return;
+          
+      // }
+      this.loading = true;
+    
+      this.backendService.donorProfile(this.editProfileForm.value)
+          .pipe(first())
+          .subscribe(
+              data => {
+                  this.alertService.success('Settings applied', true);
+                  this.router.navigate(['/setting']);
+                
+              },
+              error => {
+                  this.alertService.error(error);
+                  this.loading = false;
+              });
+    }
 }
